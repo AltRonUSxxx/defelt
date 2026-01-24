58ce9ef0165b46ff45dbee2e0f1ff6c1720f8362
Alt <ubusievnaran@gmail.com>
2026-01-24 20:17:21 +0300

    Init project

---
+++ teacher/authorization.Designer.cs
@@ -0,0 +1,199 @@
+﻿namespace teacher
+{
+    partial class authorization
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(authorization));
+            this.pictureBox_uppestPanel = new System.Windows.Forms.PictureBox();
+            this.textBox_username = new System.Windows.Forms.TextBox();
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.textBox_password = new System.Windows.Forms.TextBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox5 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button_login = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox_uppestPanel)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox_uppestPanel
+            // 
+            this.pictureBox_uppestPanel.BackColor = System.Drawing.SystemColors.Control;
+            this.pictureBox_uppestPanel.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox_uppestPanel.Name = "pictureBox_uppestPanel";
+            this.pictureBox_uppestPanel.Size = new System.Drawing.Size(400, 50);
+            this.pictureBox_uppestPanel.TabIndex = 1;
+            this.pictureBox_uppestPanel.TabStop = false;
+            this.pictureBox_uppestPanel.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pictureBox_uppestPanel_MouseDown);
+            this.pictureBox_uppestPanel.MouseMove += new System.Windows.Forms.MouseEventHandler(this.pictureBox_uppestPanel_MouseMove);
+            // 
+            // textBox_username
+            // 
+            this.textBox_username.Font = new System.Drawing.Font("Comic Sans MS", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBox_username.Location = new System.Drawing.Point(123, 283);
+            this.textBox_username.Multiline = true;
+            this.textBox_username.Name = "textBox_username";
+            this.textBox_username.Size = new System.Drawing.Size(190, 51);
+            this.textBox_username.TabIndex = 2;
+            // 
+            // pictureBox4
+            // 
+            this.pictureBox4.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox4.Image")));
+            this.pictureBox4.Location = new System.Drawing.Point(67, 284);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Size = new System.Drawing.Size(50, 50);
+            this.pictureBox4.TabIndex = 3;
+            this.pictureBox4.TabStop = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(67, 364);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(50, 50);
+            this.pictureBox1.TabIndex = 5;
+            this.pictureBox1.TabStop = false;
+            // 
+            // textBox_password
+            // 
+            this.textBox_password.Font = new System.Drawing.Font("Comic Sans MS", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBox_password.Location = new System.Drawing.Point(123, 363);
+            this.textBox_password.Multiline = true;
+            this.textBox_password.Name = "textBox_password";
+            this.textBox_password.PasswordChar = '*';
+            this.textBox_password.Size = new System.Drawing.Size(190, 51);
+            this.textBox_password.TabIndex = 4;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BackColor = System.Drawing.Color.Red;
+            this.pictureBox2.Location = new System.Drawing.Point(0, 190);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(400, 25);
+            this.pictureBox2.TabIndex = 6;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox5
+            // 
+            this.pictureBox5.BackColor = System.Drawing.Color.Red;
+            this.pictureBox5.Location = new System.Drawing.Point(375, 212);
+            this.pictureBox5.Name = "pictureBox5";
+            this.pictureBox5.Size = new System.Drawing.Size(25, 300);
+            this.pictureBox5.TabIndex = 7;
+            this.pictureBox5.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Comic Sans MS", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(110, 90);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(181, 67);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "DefElt";
+            // 
+            // button_login
+            // 
+            this.button_login.BackColor = System.Drawing.Color.Red;
+            this.button_login.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button_login.ForeColor = System.Drawing.Color.White;
+            this.button_login.Location = new System.Drawing.Point(141, 449);
+            this.button_login.Name = "button_login";
+            this.button_login.Size = new System.Drawing.Size(115, 43);
+            this.button_login.TabIndex = 9;
+            this.button_login.Text = "Войти";
+            this.button_login.UseVisualStyleBackColor = false;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.label2.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.label2.Font = new System.Drawing.Font("Comic Sans MS", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.ForeColor = System.Drawing.Color.Red;
+            this.label2.Location = new System.Drawing.Point(363, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(37, 38);
+            this.label2.TabIndex = 10;
+            this.label2.Text = "X";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // authorization
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 19F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(400, 600);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button_login);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox5);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.textBox_password);
+            this.Controls.Add(this.pictureBox4);
+            this.Controls.Add(this.textBox_username);
+            this.Controls.Add(this.pictureBox_uppestPanel);
+            this.Font = new System.Drawing.Font("Comic Sans MS", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "authorization";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Авторизация";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.authorization_FormClosing);
+            this.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.authorization_KeyPress);
+            this.Leave += new System.EventHandler(this.authorization_Leave);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox_uppestPanel)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.PictureBox pictureBox_uppestPanel;
+        private System.Windows.Forms.TextBox textBox_username;
+        private System.Windows.Forms.PictureBox pictureBox4;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.TextBox textBox_password;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox5;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button_login;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ teacher/authorization.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace teacher
+{
+    public partial class authorization : Form
+    {
+        bool allowClose = false;
+        string language = "ru";
+        public authorization()
+        {
+            InitializeComponent();
+            setLanguage();
+
+            BackgroundWorker Taskmgr_killer = new BackgroundWorker();
+            Taskmgr_killer.DoWork += Taskmgr_killer_DoWork;
+            Taskmgr_killer.RunWorkerAsync();
+        }
+
+        private void Taskmgr_killer_DoWork(object sender, DoWorkEventArgs e)
+        {
+            BackgroundWorker Taskmgr_killer = sender as BackgroundWorker;
+            while (!Taskmgr_killer.CancellationPending)
+            {
+                Process[] processes = Process.GetProcesses();
+                foreach (Process process in processes)
+                {
+                    if (process.ProcessName.ToLower().Contains("taskmgr"))
+                    {
+                        try
+                        {
+                            process.Kill();
+                        }
+                        catch
+                        {
+
+                        }
+                    }
+                }
+            }
+
+        }
+        private void setLanguage()
+        {
+            switch (language)
+            {
+                case "ru":
+                    button_login.Text = "Войти";
+                    break;
+                case "en":
+                    button_login.Text = "Login";
+                    break;
+            }
+        }
+        private void alarmClose()
+        {
+            switch (language)
+            {
+                case "ru":
+                    MessageBox.Show("Программу нельзя закрыть");
+                    break;
+                case "en":
+                    MessageBox.Show("You can't close this app");
+                    break;
+            }
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+            alarmClose();
+        }
+
+        Point lastPoint;
+        private void pictureBox_uppestPanel_MouseDown(object sender, MouseEventArgs e)
+        {
+            lastPoint = new Point(e.X, e.Y);
+            Thread.Sleep(10);
+        }
+
+        private void pictureBox_uppestPanel_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                this.Left += e.X - lastPoint.X;
+                this.Top += e.Y - lastPoint.Y;
+            }
+            Thread.Sleep(10);
+        }
+
+        private void authorization_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if(!allowClose)
+            {
+                e.Cancel = true;
+                alarmClose();
+            }
+        }
+
+        private void authorization_Leave(object sender, EventArgs e)
+        {
+            MessageBox.Show("es");
+        }
+
+        private void authorization_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if(e.KeyChar == (char)Keys.F15)
+            {
+                allowClose = !allowClose;
+            }
+        }
+    }
+}
